10a57164b30c71690b17973d42f52620272e92a6
Raphael Alencar <raphalencar@gmail.com>
2019-03-13 13:10:28 -0300

    wsgi

--- wsgi.py
+++ wsgi.py
@@ -6,7 +6,4 @@ print(" Loading keras model and Flask starting server... ")
 
 input_shape = (155, 220, 3)
 load_model(input_shape)
-
-host = '0.0.0.0'
-port = int(os.environ.get('PORT', 5000))
-application.run(host=host, port=port)
+application.run()
