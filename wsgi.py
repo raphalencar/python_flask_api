355c4059a2937f48e615b55c0817df782b4cda8a
Raphael Alencar <raphalencar@gmail.com>
2019-03-13 11:21:07 -0300

    wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,6 @@
 import os
-import logging
-from run_keras_server import app as application
+from run_keras_server import app as application 
 
-if __name__ == "wsgi":	
-	port = int(os.environ.get('PORT', 5000))
-	print(port)
-	application.run(host='0.0.0.0', port=port)
+host = '0.0.0.0'
+port = int(os.environ.get('PORT', 5000))
+application.run(host=host, port=port)
